872d5033f287453d7bcb53c9093a884e602b6abc
Patrick James <rickcjr@outlook.com>
2017-04-03 16:52:12 -0400

    Removed js file
    Removed JS file from root and placed it in the js folder

--- js/new.js
+++ js/new.js
@@ -3,3 +3,6 @@ var start = '{"favcolor":"Blue", "favSeason":"Fall"}';
 
 var myObject = JSON.parse(start);
 console.log(myObject);
+
+var myString = JSON.stringify(myObject);
+console.log(myString);
--- new.js
+++
@@ -1,8 +0,0 @@
-//JavaScript Document
-var start = '{"favcolor":"Blue", "favSeason":"Fall"}';
-
-var myObject = JSON.parse(start);
-console.log(myObject);
-
-var myString = JSON.stringify(myObject);
-console.log(myString);
