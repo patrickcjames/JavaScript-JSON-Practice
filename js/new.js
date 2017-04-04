10a7d3c6df4bfa3854adb56de270ce4e41ba0dfe
Patrick James <rickcjr@outlook.com>
2017-04-04 04:58:25 -0400

    HTTP Request
    Added HTTP Request to learn how to use AJAX calls on JSON files

--- js/new.js
+++ js/new.js
@@ -1,7 +1,34 @@
 //JavaScript Document
 
-var theData1 = '{"first":"Patrick", "last":"James", "city":"Brooklyn"}';
-var myObj = JSON.parse(theData1);
-console.log(myObj);
+// var theData1 = '{"first":"Patrick", "last":"James", "city":"Brooklyn"}';
+// var myObj = JSON.parse(theData1);
+// console.log(myObj);
+//
+// document.getElementById('message').innerHTML = myObj.city;
 
-document.getElementById('message').innerHTML = myObj.last;
+// var theData2 =
+//     '{"James":{"age":"29","degree":{"AAS":"VMI", "BA":"UVA"}},"Joseph":{"age":"49","degree":"Masters"}}';
+//
+// var myObj = JSON.parse(theData2);
+// console.log(myObj);
+//
+// document.getElementById('message').innerHTML = myObj.James.degree.BA;
+
+var xhr = new XMLHttpRequest();
+xhr.open('Get', 'data.json', true);
+
+//activates open
+xhr.send();
+
+//Track what is going on
+xhr.onreadystatechange = function() {
+  console.log(xhr.readyState);
+  console.log(xhr.statusText);
+} //end function
+
+xhr.onload = function() {
+  if (xhr.status === 200) {
+  var myStuff = JSON.parse(xhr.responseText);
+  console.log(myStuff);
+  } //end if
+} //end onload function
